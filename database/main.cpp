a3dea25c0e2311117c5e83f2f692cd5ebb802523
yichen611 <karen19950611@gmail.com>
2017-11-08 14:39:40 -0600

    Initial Commit

---
+++ database/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  database
+//
+//  Created by Yijiao Chen on 11/8/17.
+//  Copyright © 2017 Yijiao Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
